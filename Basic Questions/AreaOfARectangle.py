967b1a6f6265bbd48c854703f5609a5b67e3d2df
kushalmittal04 <kushalmittal2022@gmail.com>
2024-06-29 07:11:37 +0530

    1 basic Question done

---
+++ Basic Questions/AreaOfARectangle.py
@@ -0,0 +1,12 @@
+"""
+Write a program to calculate the area of the Rectangle.
+Note: Area of Rectangle = Length of Rectangle * Breadth of Rectangle
+"""
+
+print("Enter the Length of the Rectangle:")
+length = int(input())
+print("Enter the Breadth of the Rectangle:")
+breadth = int(input())
+
+area = length*breadth
+print("Area of the Rectangle is " + str(area))
